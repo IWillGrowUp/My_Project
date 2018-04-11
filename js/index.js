c3524d584c04bbbf96ac9e0063935f961a689a4b
Mike <mikhail.romanishin@gmail.com>
2018-04-11 08:35:13 +0000

    11/04

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-document.getElementById('found_first').addEventListener('click', function(e) {
+/*document.getElementById('found_first').addEventListener('click', function(e) {
     alert("What's Your Name");
 });
 
 document.getElementById('found_second').addEventListener('click', function (e) {
     window.open("first_found_new_window.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=200,left=500,width=288,height=600");
-});
+});*/

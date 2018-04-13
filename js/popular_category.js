b46c986c72857e6c86271de3826727fb86962393
Mike <mikhail.romanishin@gmail.com>
2018-04-13 13:53:26 +0000

    16/53

--- js/popular_category.js
+++ js/popular_category.js
@@ -1,6 +1,5 @@
-document.getElementById('ajax_phones')
-    .addEventListener('click', phones);
-    
+document.getElementById('ajax_phones').addEventListener('click', phones);
+
 function phones() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -9,14 +8,13 @@ function phones() {
                 .innerHTML = xhr.responseText;
         }
     };
-    xhr.open('GET', 'popular_category-phones.html?r='+Math.random(), true);
+    xhr.open('GET', 'popular_category-phones.html?r=' + Math.random(), true);
     xhr.send();
 }
 
-document.getElementById('ajax_bags')
-    .addEventListener('click', bags);
-    
-function bags() {
+document.getElementById('ajax_bags').addEventListener('click', numbers);
+
+function numbers() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -24,6 +22,20 @@ function bags() {
                 .innerHTML = xhr.responseText;
         }
     };
-    xhr.open('GET', 'popular_category-bags.html?r='+Math.random(), true);
+    xhr.open('GET', 'popular_category-carNumbers.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('ajax_cards').addEventListener('click', cards);
+
+function cards() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular')
+                .innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-cards.html?r=' + Math.random(), true);
     xhr.send();
 }

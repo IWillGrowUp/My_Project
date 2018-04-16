4aa3954f9cd82e91e1d642a32285014482012569
Mike <mikhail.romanishin@gmail.com>
2018-04-16 11:01:53 +0000

    16/04

--- js/popular_category.js
+++ js/popular_category.js
@@ -1,6 +1,7 @@
 document.getElementById('ajax_phones').addEventListener('click', phones);
 
 function phones() {
+    document.getElementById('close_all').style.display = "block";
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -15,6 +16,7 @@ function phones() {
 document.getElementById('ajax_bags').addEventListener('click', numbers);
 
 function numbers() {
+    document.getElementById('close_all').style.display = "block";
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
@@ -29,13 +31,103 @@ function numbers() {
 document.getElementById('ajax_cards').addEventListener('click', cards);
 
 function cards() {
+    document.getElementById('close_all').style.display = "block";
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById('new_popular')
-                .innerHTML = xhr.responseText;
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
         }
     };
     xhr.open('GET', 'popular_category-cards.html?r=' + Math.random(), true);
     xhr.send();
+}
+
+document.getElementById('ajax_rings').addEventListener('click', rings);
+
+function rings() {
+    document.getElementById('close_all').style.display = "block";
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-gold.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('ajax_documents').addEventListener('click', documents);
+
+function documents() {
+    document.getElementById('close_all').style.display = "block";
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-documents.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('ajax_flash').addEventListener('click', flash);
+
+function flash() {
+    document.getElementById('close_all').style.display = "block";
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-flash.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('ajax_keys').addEventListener('click', keys);
+
+function keys() {
+    document.getElementById('close_all').style.display = "block";
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-keys.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('ajax_animals').addEventListener('click', animals);
+
+function animals() {
+    document.getElementById('close_all').style.display = "block";
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-animals.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('ajax_another').addEventListener('click', another);
+
+function another() {
+    document.getElementById('close_all').style.display = "block";
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('new_popular').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.open('GET', 'popular_category-another.html?r=' + Math.random(), true);
+    xhr.send();
+}
+
+document.getElementById('close_all').addEventListener('click', close);
+function close() {
+    document.getElementById('new_popular').innerHTML = ' ';
+    document.getElementById('close_all').style.display = "none";
 }
